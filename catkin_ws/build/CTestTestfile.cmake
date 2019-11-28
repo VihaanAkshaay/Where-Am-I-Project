a37178bf481904655479c52b74527ac5e8ca8045
VihaanAkshaay <akswins001@gmail.com>
2019-11-29 01:53:10 +0530

    added a gazebo model of a basic bot structure and made launch files for worlds

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vihaan/Workspace/Where-Am-I-Project/catkin_ws/src
+# Build directory: /home/vihaan/Workspace/Where-Am-I-Project/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("udacity_bot")
